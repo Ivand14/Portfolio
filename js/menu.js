6b23ee808456038ea2a5e69d068634a80a6245d3
Ivan <carivan14@homtail.com>
2023-02-17 16:36:50 -0300

    mejoras en nav

--- js/menu.js
+++ js/menu.js
@@ -23,3 +23,17 @@ btnActive.forEach(btn=>{
 })
 
 
+const seccion= document.querySelectorAll('.seccion')
+const items= document.querySelectorAll('.items')
+
+
+function activeMenu(){
+    let len=seccion.length;
+    while(--len && window.scrollY + 97 < seccion[len].offsetTop){}
+    items.forEach(it=>it.classList.remove('active'));
+    items[len].classList.add('active')
+}
+
+activeMenu()
+
+window.addEventListener('scroll',activeMenu)
